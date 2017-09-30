f06701e82430189151a124128e815414032569b5
Ruslan Akberov <ruslanakberov@gmail.com>
2017-09-30 20:32:13 +0300

    Initial Commit

---
+++ SizeClasses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClasses
+//
+//  Created by Руслан Акберов on 30.09.2017.
+//  Copyright © 2017 Руслан Акберов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
